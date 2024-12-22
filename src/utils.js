ebffe3dd33bb10073cfd19776eb8145c4db574a4
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-12-22 22:29:03 +0100

    ci: debug

--- src/utils.js
+++ src/utils.js
@@ -8,6 +8,7 @@ export const isEqual = (obj1, obj2) => JSON.stringify(obj1) === JSON.stringify(o
 
 export const isBoolean = (value) => typeof value === "boolean";
 
+
 function flat(arr, target) {
   arr.forEach(function (el) {
     if (Array.isArray(el)) flat(el, target);
